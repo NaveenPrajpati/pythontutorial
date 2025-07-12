e96d40fa1cdb15fc40d7f9d6ffe8dff73609e2b1
naveenprajpati <solidauto202@gmail.com>
2025-07-12 18:41:13 +0530

    variable added

---
+++ variables.py
@@ -0,0 +1,17 @@
+a=2  #a is integer
+b=3
+c=3.5 # c is floating point number
+d=None # d ia a none type variable
+
+e = False and False
+f="naveen"
+
+print(a+b , e , not(False), type(f),type(str(a)),len(f))
+
+
+
+
+# in1 = int(input("Enter a number 1 "))
+# in2 = int(input("Enter a number 2 "))
+
+# print(in1 + in2)
